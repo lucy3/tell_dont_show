43e1f8c550893d0f32aca897df3681c65655e3b0
Lucy Li <luxisufeili@gmail.com>
2025-05-28 14:22:29 -0700

    Update topic_modeling.sh

--- topic_modeling.sh
+++ topic_modeling.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 set -e
 
-PREFIX=describe_gpt-4o_0 # name of your experiment, like passage_type in preprocess_text_for_lda.py
+PREFIX=describe_gpt-4o # name of your experiment, corresponding to passage_type in preprocess_text_for_lda.py
 
-/mnt/data0/lucy/Mallet-202108/bin/mallet import-file --input /mnt/data0/lucy/literary-theme-analysis/outputs/topic_modeling/"$PREFIX"_all_input_docs.tsv --output /mnt/data0/lucy/literary-theme-analysis/outputs/topic_modeling/"$PREFIX"_input_text.mallet --keep-sequence
+/path/to/your/Mallet-202108/bin/mallet import-file --input /path/to/your/"$PREFIX"_all_input_docs.tsv --output /path/to/your/"$PREFIX"_input_text.mallet --keep-sequence
 
-bash topic_modeling_helper.sh /mnt/data0/lucy/Mallet-202108/bin /mnt/data0/lucy/literary-theme-analysis/outputs/topic_modeling 100 "$PREFIX"
+bash topic_modeling_helper.sh /path/to/your/Mallet-202108/bin /path/to/your/outputs/topic_modeling 100 "$PREFIX"
